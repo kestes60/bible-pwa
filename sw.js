f82c59056841f356dd2bd3c76511103517b64797
Keith Estes <kestes60@yahoo.com>
2025-12-11 20:37:26 -0500

    Add full Yahtsar Dove & Map icon set to assets (14 sizes maskable) + manifest/SW v40

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cache version constant - increment this to bust the cache
-const CACHE_VERSION = "bible-pwa-v39";
+const CACHE_VERSION = "bible-pwa-v40";
 
 // Get the base path from the service worker's own location
 // e.g., if SW is at /bible-pwa/sw.js, base is /bible-pwa/
